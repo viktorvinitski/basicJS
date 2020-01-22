ba1f959cab96c985a98a406afa7d988a720338b0
Viktor Vinitski <fireworkv0@gmail.com>
2020-01-22 17:54:33 +0300

    commit message

--- Задача 5.js
+++ Задача 5.js
@@ -2,10 +2,10 @@ let password = prompt("Введите PIN", "4 или 6 цифр")
 
 function PIN(){
 if ((password.length == 4 || password.length == 6) && parseInt(password)){
-    return(password)
+    return true
 }
 else{
-    alert(false)
+    return false
 }
 }
 PIN();
