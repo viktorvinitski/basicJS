5517f599ffdd2009e8be544bcd62acf6c2762a40
Viktor Vinitski <fireworkv0@gmail.com>
2020-02-07 00:28:04 +0300

    commit message

---
+++ ARRAY/array.js
@@ -0,0 +1,7 @@
+let arr = ['a', 'b', 'c']
+alert(arr)
+
+
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
--- Задача 4.js
+++ Задача 4.js
@@ -12,21 +12,6 @@ console.log(newString)
 
 
 
-// let mainString = 
-//     newString[0].toUpperCase() + "-" +
-//     newString[1].toUpperCase() + newString[1].repeat(1).toLowerCase() + '-' +
-//     newString[2].toUpperCase() + newString[2].repeat(2).toLowerCase() + '-' + 
-//     newString[3].toUpperCase() + newString[3].repeat(3).toLowerCase() + '-' +
-//     newString[4].toUpperCase() + newString[4].repeat(4).toLowerCase() + '-' +
-//     newString[5].toUpperCase() + newString[5].repeat(5).toLowerCase() + '-' +
-//     newString[6].toUpperCase() + newString[6].repeat(6).toLowerCase() 
-// alert(mainString)
-
-
-
-//accum("abcd") -> "A-Bb-Ccc-Dddd"
-//accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
-//accum("cwAt") -> "C-Ww-Aaa-Tttt"
 
 
 
